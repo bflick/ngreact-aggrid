fc70bf8b80111ae48ff280e9e96fe25a6c8e3945
Brian Flick <brian.flick21@gmail.com>
2016-07-03 23:18:27 -0500

    Initial commit runs into a loop : :

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack'),
+  port = 3000;
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:' + port,
+    'webpack/hot/only-dev-server',
+    './lib/app.js'
+  ],
+  output: {
+    'path': './app/',
+    'filename': 'app.js',
+    'publicPath': '/'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', 'babel']}
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devtool: 'cheap-source-map',
+  devServer: {
+    port: port,
+    info: false,
+    historyApiFallback: true,
+    hot: true,
+    contentBase: './app',
+    host: 'localhost'
+  },
+  resolve: {
+    alias: {
+      "ag-grid-root" : __dirname + "/node_modules/ag-grid"
+    }
+  }
+};
